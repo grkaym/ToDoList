7c2dad8c46768bd09c78bfd01adf1adec1d3efd9
KAZUKI IDA <108783949+GorikiAyame@users.noreply.github.com>
2023-02-03 22:23:49 +0900

    タスク作成機能実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Models\Folder;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 
 
@@ -25,6 +26,31 @@ class TaskController extends Controller
             'folders' => $folders,
             'current_folder_id' => $current_folder->id,
             'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/create
+     */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
--- resources/lang/jp/validation.php
+++ resources/lang/jp/validation.php
@@ -130,6 +130,8 @@ return [
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
 
+    'date'                 => ':attribute には日付を入力してください。',
+
     /*
     |--------------------------------------------------------------------------
     | Custom Validation Language Lines
